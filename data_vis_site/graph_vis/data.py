2481e38395ce9a4efbc7b12883ebff01ec497881
perry-C <2558544545qwerty@gmail.com>
2023-05-03 22:24:35 +0100

    Feat: adjustments to map

--- data_vis_site/graph_vis/data.py
+++ data_vis_site/graph_vis/data.py
@@ -4,44 +4,53 @@ from sqlalchemy import create_engine
 
 from .models import GraphVisRefugeeData
 
-'''
+"""
 This file contains functions of that deal with data 
 of Asylum-seeker data between 2015-2022 from UNHCR website 
-'''
+"""
 
 
 def convert_csv_to_df(file_path):
-    '''
+    """
     Auxiliary function that converts csv file to pandas dataframe format,
     used in combination with the "add_data" command to for constructing the database,
     see "add_data.py" for more detail
 
     NOTE: this is a brute-force implementation which only works for this particular dataset
-    '''
+    """
 
     with open(file_path) as fp:
         # Trim down the description text
-        pop_df = pd.read_csv(fp, skip_blank_lines=True,
-                             skiprows=range(14), on_bad_lines="error", sep=",")
+        pop_df = pd.read_csv(
+            fp, skip_blank_lines=True, skiprows=range(14), on_bad_lines="error", sep=","
+        )
 
     pop_df.head()
 
     # We ignore the IDPs in this scenarios, as this would complicate our graph visualisation
 
-    pop_df = pop_df[pop_df["Country of origin (ISO)"]
-                    != pop_df["Country of asylum (ISO)"]]
+    pop_df = pop_df[
+        pop_df["Country of origin (ISO)"] != pop_df["Country of asylum (ISO)"]
+    ]
 
     pop_df.drop(columns=["IDPs of concern to UNHCR"], inplace=True)
 
     # key_feat_df = pop_df.filter(
     #     items=["Year", "Country of origin", "Country of asylum", "Asylum-seekers"])
 
-    refugee_types = ["Country of asylum (ISO)", "Refugees under UNHCR's mandate", "Asylum-seekers",
-                     "Other people in need of international protection", "Stateless persons", "Host Community", "Others of concern"]
+    refugee_types = [
+        "Country of asylum (ISO)",
+        "Refugees under UNHCR's mandate",
+        "Asylum-seekers",
+        "Other people in need of international protection",
+        "Stateless persons",
+        "Host Community",
+        "Others of concern",
+    ]
 
     pop_df["total_refugees"] = pop_df[refugee_types].sum(axis=1)
 
     # Lastly, adding the indices for each record so they could be used as primary keys
     pop_df.insert(0, "id", range(1, len(pop_df) + 1))
-    
+
     return pop_df
--- data_vis_site/graph_vis/graphs.py
+++ data_vis_site/graph_vis/graphs.py
@@ -1,10 +1,12 @@
-'''
+"""
 This file contains a number of functions to generate pyplot graph in the form of html strings,
 which can then be used for rendering onto template webpages by 'views.py'
-'''
+"""
 
 
+import numpy as np
 import pandas as pd
+import plotly.colors as pc
 import plotly.express as px
 import plotly.graph_objs as go
 from django.http import HttpResponse
@@ -15,12 +17,12 @@ from .models import GraphVisRefugeeData
 
 
 def make_bar_chart():
-    '''
+    """
     Returns:
         str: a html div string with the a bar chart enclosed inside
-    '''
+    """
 
-    column_names = ['country_of_origin_iso', 'total_refugees', 'year']
+    column_names = ["country_of_origin_iso", "total_refugees", "year"]
 
     qs = GraphVisRefugeeData.objects.values_list(*column_names)
 
@@ -28,13 +30,17 @@ def make_bar_chart():
 
     fig = go.Figure()
 
-    for country in df['country_of_origin_iso'].unique()[:5]:
-        country_df = df.loc[df['country_of_origin_iso'] == country]
+    for country in df["country_of_origin_iso"].unique()[:5]:
+        country_df = df.loc[df["country_of_origin_iso"] == country]
         fig.add_trace(
-            go.Histogram(x=country_df.loc[:, 'year'], y=country_df.loc[:, 'total_refugees'], name=country))
+            go.Histogram(
+                x=country_df.loc[:, "year"],
+                y=country_df.loc[:, "total_refugees"],
+                name=country,
+            )
+        )
 
-    bar_chart = plot(fig,
-                     output_type='div')
+    bar_chart = plot(fig, output_type="div")
 
     return bar_chart
 
@@ -43,15 +49,21 @@ def make_world_map():
     # df = pd.read_csv(
     #     'https://raw.githubusercontent.com/plotly/datasets/master/2014_world_gdp_with_codes.csv')
 
-    column_names = ['country_of_asylum', 'country_of_origin_iso',
-                    'country_of_asylum_iso', 'total_refugees', 'year']
+    column_names = [
+        "country_of_asylum",
+        "country_of_origin_iso",
+        "country_of_asylum_iso",
+        "total_refugees",
+        "year",
+    ]
 
     # for now just manually filter cooi to be AFG
 
     # TODO: later change the ISO to be soemthing selectable
 
-    qs = GraphVisRefugeeData.objects.values_list(
-        *column_names).filter(country_of_origin_iso='AFG')
+    qs = GraphVisRefugeeData.objects.values_list(*column_names).filter(
+        country_of_origin_iso="AFG"
+    )
 
     df = pd.DataFrame(qs, columns=column_names)
 
@@ -59,60 +71,115 @@ def make_world_map():
 
     fig = go.Figure()
 
-    start_year = df['year'].min()
-    end_year = df['year'].max()
+    start_year = df["year"].min()
+    end_year = df["year"].max()
+
+    figs = []
 
     for y in range(start_year, end_year + 1):
+        filtered_df = df[df["year"] == y]
 
-        filtered_df = df[df['year'] == y]
+        total_refugees = filtered_df["total_refugees"]
 
-        fig.add_trace(go.Choropleth(
-            locations=filtered_df['country_of_asylum_iso'],
-            z=filtered_df['total_refugees'],
-            text=filtered_df['country_of_asylum'],
-            colorscale="Viridis",
+        total_refugees_pct = total_refugees / sum(total_refugees)
+
+        # TODO: change this to percentage scale
+
+        total_refugees_pct_log = np.log10(total_refugees_pct)
+
+        ticktext = np.geomspace(total_refugees_pct.min(), total_refugees_pct.max(), 10)
+
+        ticktext = [float(x) * 100 for x in ticktext]
+
+        ticktext = [
+            np.format_float_positional(
+                x, precision=1, unique=False, fractional=False, trim="k"
+            )
+            for x in ticktext
+        ]
+
+        ticktext = [f"{x}%" for x in ticktext]
+
+        tickvals = np.geomspace(
+            total_refugees_pct_log.min(), total_refugees_pct_log.max(), 10
+        )
+
+        choropleth = go.Choropleth(
+            locations=filtered_df["country_of_asylum_iso"],
+            z=total_refugees_pct_log,
+            text=total_refugees_pct,
+            colorscale="viridis",
             autocolorscale=False,
             reversescale=True,
-            marker_line_color='darkgray',
+            colorbar=dict(
+                title={
+                    "text": "Refugee Count",
+                    "font": dict(
+                        size=15,
+                    ),
+                },
+                x=0.95,
+                tickvals=tickvals,
+                ticktext=ticktext,
+            ),  
+            marker_line_color="darkgray",
             marker_line_width=1.2,
-            # colorbar_tickprefix='',
-            colorbar_title='Refugee Count',
             visible=False,
-            hoverinfo='location+z+text'
+            hoverinfo="location+text",
+        )
 
-        ))
+        figs.extend([choropleth])
+
+    fig.add_traces(figs)
+
+    # Highlight origin country on the map
+    fig.add_trace(
+        go.Choropleth(
+            locations=["AFG"],
+            z=[0],
+            colorscale=[[0, "red"], [1, "red"]],
+            colorbar=None,
+            showscale=False,
+            visible=True,
+            hoverinfo="location",
+        ),
+    )
 
     # Show newest data by default
-    fig.data[-1].visible = True
+    fig.data[-2].visible = True
 
     # Create and add slider
-    base_title = "Amount of Afghanistan Rufugees Seeking Shelters Around the Globe (2015-2022)"
+    base_title = "The proportion of Afghan Refugees settling in countries around the world (2015-2022)"
     steps = []
     for i in range(len(fig.data)):
-
-        title = f'{base_title} - Year: {start_year + i}'
+        title = f"{base_title} - Year: {start_year + i}"
         step = dict(
             method="update",
-            args=[{"visible": [False] * len(fig.data)},
-                  {"title": title}],  # layout attribute
+            args=[
+                {"visible": [False] * len(fig.data)},
+                {"title": title},
+            ],  # layout attribute
             label=str(start_year + i),
         )
         step["args"][0]["visible"][i] = True  # Toggle i'th trace to "visible"
-        steps.append(step)
+        step["args"][0]["visible"][-1] = True  # Toggle AFG (last one) to "visible"
 
-    sliders = [dict(
-        active=end_year-start_year,
-        currentvalue={"prefix": "Current Year: "},
-        pad={"t": 50},
-        steps=steps,
-    )]
+        if len(steps) != len(fig.data) - 1:
+            steps.append(step)
+
+    sliders = [
+        dict(
+            active=end_year - start_year,
+            currentvalue={"prefix": "Current Year: "},
+            pad={"t": 50},
+            steps=steps,
+        )
+    ]
 
     fig.update_layout(
         title_text=base_title,
         geo=dict(
-            showframe=True,
-            showcoastlines=False,
-            projection_type='equirectangular'
+            showframe=True, showcoastlines=False, projection_type="equirectangular"
         ),
         # annotations=[dict(
         #     x=0.55,
@@ -123,16 +190,16 @@ def make_world_map():
         #             UNHCR Refugee Data Finder</a>',
         #     showarrow=False
         # )],
-        sliders=sliders
-
+        sliders=sliders,
     )
 
     # fig.update_traces(hovertemplate=f'<extra></extra>')
 
-    world_map = plot(fig,
-                     output_type='div',
-                     )
+    world_map = plot(
+        fig,
+        output_type="div",
+    )
 
-    world_map = world_map.replace('<div', '<div class="graph"')
+    world_map = world_map.replace("<div", '<div class="graph"')
 
     return world_map
